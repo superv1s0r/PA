0164007e44c1409bbfa8f9a3b1270c754a20b760
superv1s0r <oleksandr.baranets1@gmail.com>
2024-10-18 21:45:32 +0200

    Less files ej4

--- epd3/ej4/ej4.php
+++ epd3/ej4/ej4.php
@@ -1,6 +1,4 @@
-<a href="ej4.html">Átras</a>
-<br>
-<?php
+<?php 
 $almacen = [
     1 => [
         'a' => [
@@ -25,28 +23,56 @@ $almacen = [
     ]
 ];
 
-$sku_recibido = $_GET['SKU'];
-$sku_recibido = strtoupper($sku_recibido);
-
-$matches = 0;
-
-foreach ($almacen as $pasillo => $estantes) {
-    foreach ($estantes as $estante => $productos) {
-        $cantidad_aux = 0;
-        foreach ($productos as $producto) {
-
-            if ($producto['SKU'] == $sku_recibido) {
-                $cantidad_aux += $producto['Cantidad'];
-                $matches += 1;    
-
-            }
-        }
-        if($cantidad_aux > 0)
-            echo "Pasillo $pasillo - Estante $estante - $cantidad_aux unidades </br>";
-        
-    }
-}
-    if($matches == 0)
-        echo "No se ha encontrado ningun articulo relacionado con " . $sku_recibido;
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="ej4.css" />
+
+</head>
+<body>
+
+    <img src="img.png" class="img">
+
+    <form action="ej4.php" method="get">
+
+        <input type="text" name="SKU" required>
+        <input type="submit" value="Submit">
+
+    </form>
+<br>
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['SKU'])) {
+
+    $sku_recibido = $_GET['SKU'];
+    $sku_recibido = strtoupper($sku_recibido); 
+    $matches = 0;
+    
+    foreach ($almacen as $pasillo => $estantes) {
+        foreach ($estantes as $estante => $productos) {
+            $cantidad_aux = 0;
+            foreach ($productos as $producto) {
+    
+                if ($producto['SKU'] == $sku_recibido) {
+                    $cantidad_aux += $producto['Cantidad'];
+                    $matches += 1;    
+    
+                }
+            }
+            if($cantidad_aux > 0)
+                echo "Pasillo $pasillo - Estante $estante - $cantidad_aux unidades </br>";
+            
+        }
+    }
+        if( $matches == 0){
+            echo "No se ha encontrado ningun articulo relacionado con " . $sku_recibido;
+        }
+}
+
+?>
+</body>
+</html>
+
 
