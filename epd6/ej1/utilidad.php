f38f883742832c4c3826dbd6016a5092dd8cdba8
superv1s0r <oleksandr.baranets1@gmail.com>
2024-11-24 20:10:50 +0100

    Utilidad y Config actualizados, para tener clases

---
+++ epd6/ej1/utilidad.php
@@ -0,0 +1,31 @@
+<?php
+//Clase para las funciones frecuentemente usadas
+class Helper {
+    public static function redirect($url) {
+        header("Location: $url");
+        exit();
+    }
+
+    public static function sanitizeInput($input) {
+        return htmlspecialchars(trim($input), ENT_QUOTES, 'UTF-8');
+    }
+
+    public static function validateFields($fields) {
+        $errors = [];
+        foreach ($fields as $field => $value) {
+            if (empty(trim($value))) {
+                $errors[$field] = "El campo $field no puede estar vacío.";
+            }
+        }
+        return $errors;
+    }
+
+    public static function formatErrors($errors) {
+        $output = "<ul style='color: red;'>";
+        foreach ($errors as $error) {
+            $output .= "<li>$error</li>";
+        }
+        $output .= "</ul>";
+        return $output;
+    }
+}
